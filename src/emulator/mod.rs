55c1abc3cd1b19278dd5af72cba57fb314a9c19a
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-27 12:13:37 +0100

    First commit: Load a program

---
+++ src/emulator/mod.rs
@@ -0,0 +1,65 @@
+use std::{alloc::Layout, ffi::FromBytesWithNulError};
+
+// In memory we should have the instructions
+enum Insn {
+    Noop,
+}
+
+mod layout {
+    /// Total number of addressable memory words.
+    /// Memory addresses range from 0 to MEM_SIZE - 1.
+    pub const MEM_SIZE: usize = 32_768;
+
+    /// Lowest valid memory address.
+    pub const MEM_MIN: usize = 0;
+
+    /// Highest valid memory address.
+    pub const MEM_MAX: usize = MEM_SIZE - 1;
+
+    /// Number of general-purpose registers.
+    pub const NUM_REGS: usize = 8;
+
+    /// Lowest address mapped to a register.
+    pub const REG_MIN: usize = MEM_SIZE;
+
+    /// Highest address mapped to a register.
+    pub const REG_MAX: usize = REG_MIN + NUM_REGS - 1;
+}
+
+pub struct Cpu {
+    pub mem: [u16; layout::MEM_SIZE], // The size will depend of the ROMs
+    pub regs: [u16; layout::NUM_REGS],
+    pub ip: u16,        // Instruction pointer
+    pub footprint: u16, // keep the program's memory footprint
+}
+
+impl Cpu {
+    pub fn load(roms: Vec<u8>) -> Cpu {
+        // The format of the ROM is each number is stored as a 16 bit little endian pair
+        // Example:
+        //   - roms[0] -> low byte of mem[0]
+        //   - roms[1] -> high byte of mem[0]
+        // ...
+        //   - roms[i] -> low byte of mem[i]
+        //   - roms[i + 1] -> high byte of mem[i]
+        // Programs are loaded into memory starting at address 0
+        assert!(roms.len() % 2 == 0, "ROMs size is odd");
+        let footprint = roms.len() / 2;
+        assert!(footprint <= layout::MEM_SIZE);
+
+        let mut cpu = Cpu {
+            mem: [0; layout::MEM_SIZE],
+            regs: [0; layout::NUM_REGS],
+            ip: 0,
+            footprint: footprint as u16,
+        };
+
+        for (idx, chunk) in roms.chunks_exact(2).enumerate() {
+            let low = chunk[0] as u16;
+            let high = (chunk[1] as u16) << 8;
+            cpu.mem[idx] = high | low;
+        }
+
+        cpu
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+mod emulator;
+
+fn main() -> io::Result<()> {
+    let mut f = File::open("roms/challenge.bin")?;
+    let mut data = vec![];
+    f.read_to_end(&mut data)?;
+
+    // Print 16 first bytes of data
+    for byte in data.iter().take(16) {
+        print!("{:02X}  ", byte);
+    }
+    println!("");
+
+    let cpu = emulator::Cpu::load(data);
+    // Print 8 first bytes of memory
+    for (idx, byte) in cpu.mem.iter().take(8).enumerate() {
+        println!("{:04X}: {:02X}  ", idx, byte);
+    }
+
+    println!("{} words loaded in memory", cpu.footprint);
+    Ok(())
+}
